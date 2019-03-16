25f6f5002fb8a100a4322ef653beacf7d6d28856
Ihar <petrushen@yahoo.com>
2019-03-16 14:40:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-module.exports = function getZerosCount(number, base=5) {
-  let endzerocount = 0;
+module.exports = function getZerosCount(number, base) 
+{ 
+    if(base%10 == 0) {
+        let endzerocount = 0;
   
   let i = 1;
-  while(Math.floor(number/(Math.pow(base,i)))>=1){
-
-  	if(number%(Math.pow(base,i) == 0) {
-      endzerocount++;
-      number = Math.floor(number/(Math.pow(base,i)));
-  } else {
-  	return endzerocount;
-  }
+  while(Math.floor(number/(Math.pow(5,i)))>=1){
+      endzerocount += Math.floor(number/(Math.pow(5,i)));
     i++;
   }
   
-  return endzerocount;
-}
+return endzerocount;
+    } 
+    let srt_number = new String(number);
+
+    let len = srt_number.length;
+    let pow = 1; 
+    let num = 0; 
+    let zeros = 0;
+
+    for (let i = len - 1; i >= 0; i--) 
+    { 
+       
+        // if (Number(srt_number[i]) >= base) 
+        // { 
+        //     alert("Invalid Number"); 
+        //     return -1; 
+        // } 
+  
+        num += Number(srt_number[i]) * pow; 
+        pow = pow * base; 
+    } 
+          while(num/base > 1) {
+            num = (num - num%base)/base;
+            zeros += num;
+          }
+    return zeros;
+} 
