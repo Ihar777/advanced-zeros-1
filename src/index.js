1a26df5be17389a7ba389f80ee7dcb4b923f4ac9
Ihar <petrushen@yahoo.com>
2019-03-16 07:09:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, base=5) {
+  let endzerocount = 0;
+  
+  let i = 1;
+  while(Math.floor(number/(Math.pow(base,i)))>=1){
+
+  	if(number%(Math.pow(base,i) == 0) {
+      endzerocount++;
+      number = Math.floor(number/(Math.pow(base,i)));
+  } else {
+  	return endzerocount;
+  }
+    i++;
+  }
+  
+  return endzerocount;
 }
