a89345be5321e058ffab7cc4a8a5d931585fc8bd
Ihar <petrushen@yahoo.com>
2019-03-24 11:22:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
-module.exports = function getZerosCount(number, base) { 
-    if(base == 10) {
-        let endzerocount = 0;
-  
-  let i = 1;
-  while(Math.floor(number/(Math.pow(5,i)))>=1){
-      endzerocount += Math.floor(number/(Math.pow(5,i)));
-    i++;
-  }
-  
-return endzerocount;
-    } else {
-    var str_number = number.toString();
+module.exports = function getZerosCount(number, base) {
+               
+                let multiplier, mult_grade_base, accum_grade, number_clone, temp_grade;
+                let result;
+                let base_clone = base;
+                // to solve the task one should devide the number by the highest multiplier 
+                // of the base (multiplier must be a prime number) recursively 
+                // and sum up the results of the division
+                // if multiplier is not a prime number, it is divided by greater than 1 mult_grade_base 
+                for (multiplier = 2; multiplier <= base; multiplier++) {
 
-    var len = str_number.length;
-    var pow = 1; // Initialize power of base 
-    var num = 0; // Initialize result 
-    var zeros = 0;
-  
-    for (var i = len - 1; i >= 0; i--) 
-    { 
-        if (Number(str_number[i]) >= base) 
-        { 
-            alert("Invalid Number"); 
-            return -1; 
-        } 
+                    if (base_clone % multiplier === 0) {
 
-         num += str_number[i] * pow;
-        pow = pow * base; 
-    } 
+                        mult_grade_base = 0;
+                        accum_grade = 0;
+                        // the accumulated highest grade of the number
+                        number_clone = number;
 
-          while(num/base > 1) {
-            num = Math.floor(num/base);
-            zeros += num;
-          }
-        
-    return zeros;
-} 
-}
+                        while (base_clone % multiplier === 0) {
+
+                            mult_grade_base++;
+                            base_clone = Math.floor(base_clone / multiplier);
+
+                        }
+                        // finding the maximum grade of the number by dividing 
+                        // the number by its multiplier and getting a sum of the division results
+                        (function grades() {
+
+                            temp_grade = Math.floor(number_clone / multiplier);
+                            accum_grade = accum_grade + temp_grade;
+                            number_clone = temp_grade;
+
+                            if (temp_grade > 0) {
+
+                                grades();
+
+                            }
+
+                        }
+                        )();
+
+                        result = Math.floor(accum_grade / mult_grade_base);
+                    }
+
+                }
+
+                return result;
+            }
